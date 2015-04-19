18dfad20b0a9d6550d385057a0f65efec6bf90e6
Felipe Valdivia <fe.valdiviaa@gmail.com>
2015-04-19 13:57:25 -0300

    Add method after sing_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  protected
+  before_action :configure_permitted_parameters, if: :devise_controller?
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << {profile_attributes: [:type]}
+      devise_parameter_sanitizer.for(:account_update) << :newsletter
+    end
+  def after_sign_in_path_for(user)
+    # return the path based on resource
+    edit_profile_path(current_user.profile)
+  end
 end

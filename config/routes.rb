166954373bc7247a2369474d777714762136f10f
Felipe Valdivia <fe.valdiviaa@gmail.com>
2015-04-19 22:59:07 -0300

    Add configuration for image profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :pets
 
   resources :profiles, only: [:show, :edit, :update]
+  resources :user_pets, only: :index
+  resources :services
 
   root 'home#index'
 end

66def9092f5ec440dd35fb1cacc29583f4253665
Felipe Valdivia <fe.valdiviaa@gmail.com>
2015-04-19 22:57:44 -0300

    Add relation betwern user and services

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :profile, inverse_of: :user, dependent: :destroy
+  has_many :pets, dependent: :destroy
+  validates :profile, presence: true
 
   accepts_nested_attributes_for :profile
+
+  def display_image
+    return profile.avatar.url if profile.avatar?
+    self.gravatar_url || '/user.png'
+  end
+
+  def gravatar_url
+    return unless self.email
+    "https://gravatar.com/avatar/#{Digest::MD5.new.update(self.email)}.jpg?default=http://#{Rails.configuration.default_url_options[:host]}/user.png"
+  end
 end

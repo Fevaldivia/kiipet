696a302975dc71db3d33720d2c6a014cde359b7e
Felipe Valdivia <fe.valdiviaa@gmail.com>
2015-04-23 00:50:09 -0300

    Fix seed information

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 load "#{Rails.root}/db/seeds/chilean_cities.rb"
-load "#{Rails.root}/db/seeds/countries.rb"

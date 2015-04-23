386f564529d91000bc52c65636432b1e8047d866
Felipe Valdivia <fe.valdiviaa@gmail.com>
2015-04-23 00:50:30 -0300

    Add models for county and region

--- app/models/county.rb
+++ app/models/county.rb
@@ -2,4 +2,5 @@ class County < ActiveRecord::Base
   belongs_to :region
 
   validates :name, presence: true, uniqueness: true
+  validates :region, presence: true
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,11 +1,13 @@
 class Region < ActiveRecord::Base
-  belongs_to :country
+  include CountryHandler
+
   has_many :counties, dependent: :destroy
 
   validates :name, :short_name, :code, presence: true, uniqueness: true
-  validates :country, presence: true
+
+  validates :country_code, presence: true
 
   def self.allowed_attributes
-    [:name, :short_name, :code]
+    [:name, :short_name, :code, :country_code]
   end
 end

91b2007dfc2c1daaf59d8e89cbe2b7da17367f9f
felipe <fe.valdiviaa@gmail.com>
2015-05-29 01:11:00 -0300

    Add bank acount type migration

---
+++ db/migrate/20150514044916_create_bank_account_types.rb
@@ -0,0 +1,9 @@
+class CreateBankAccountTypes < ActiveRecord::Migration
+  def change
+    create_table :bank_account_types do |t|
+      t.string :name
+
+      t.timestamps null: false
+    end
+  end
+end

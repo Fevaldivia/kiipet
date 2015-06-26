beb16d4c31d362ac305f3fc468780d2a4fee50da
felipe <fe.valdiviaa@gmail.com>
2015-06-26 20:33:37 -0300

    Add controller for keeper_users home index

---
+++ app/controllers/keeper_users_controller.rb
@@ -0,0 +1,9 @@
+class KeeperUsersController < ApplicationController
+    before_action :authenticate_user!
+
+  # GET /user_pets
+  def index
+    @keepers = Profile.where(type: "KeeperProfile")
+  end
+
+end

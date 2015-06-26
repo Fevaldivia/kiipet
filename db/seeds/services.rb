84ed85bc5e9c29824d1b22e820465a05c4bdc7ab
felipe <fe.valdiviaa@gmail.com>
2015-06-26 20:33:01 -0300

    Add seed for services

---
+++ db/seeds/services.rb
@@ -0,0 +1,5 @@
+puts "loading ruby #{__FILE__}"
+
+Service.create(name: 'Alojamiento', description: "Cuida perros en tu propia casa durante el día y la noche. En el horario que mas te acomode.")
+Service.create(name: 'Paseo', description: "Pasea perros durante 1 hora. La recogida y entrega será siempre en el domicilio del propietario.")
+Service.create(name: 'Cuidado Diario', description: "Cuida perros en tu propia casa sólo durante el día y por un máximo de 10 horas al día.")

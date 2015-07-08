dcf1d1dea6698b8e504adb13f09cb00279725993
felipe <fe.valdiviaa@gmail.com>
2015-07-08 15:14:05 -0300

    Fix jquery for calendar works with turbolinks

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-
-    // page is now ready, initialize the calendar...
-    jQuery('#calendar').fullCalendar({
-        // put your options and callbacks here
-        weekends: false
-    })
+$(document).on("page:load", function(){
+   // page is now ready, initialize the calendar...
+    $('#calendar').fullCalendar({
+      dayClick: function() {
+      alert('has hecho click en un dia');
+    }
+  });
 });
 

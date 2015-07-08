23bcf7cae616adb1a1b58c8ed5b90c839b4f28f6
felipe <fe.valdiviaa@gmail.com>
2015-07-08 12:58:31 -0300

    Add calendar js file for javascript

---
+++ app/assets/javascripts/calendar.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+    jQuery('#calendar').fullCalendar({
+        // put your options and callbacks here
+        weekends: false
+    })
+});
+

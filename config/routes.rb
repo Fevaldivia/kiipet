b0baecf54f5ba8b3a3bacdb7bca56a6f46ae9809
felipe <fe.valdiviaa@gmail.com>
2015-07-10 14:17:29 -0300

    Add routes for calendar API

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,11 @@ Rails.application.routes.draw do
   get "calendar", to: "calendars#index"
 
   get '/regions/:region_id/counties', to: 'counties#index', as: :counties
+
+  namespace :api, defaults: { format: :json } do
+    scope module: :v1 do
+      #routes
+      resources :calendars, only: [:index, :create]
+    end
+  end
 end

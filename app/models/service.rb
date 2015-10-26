997d6cceca1369e55a4eae0401d087928b546c92
felipe <fe.valdiviaa@gmail.com>
2015-10-26 20:23:18 -0300

    Add relationship between services and calendars

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,6 +3,9 @@ class Service < ActiveRecord::Base
   has_many :profile_services
   has_many :profiles, through: :profile_services
 
+  has_many :calendars
+  has_many :profiles, through: :calendars
+
   def self.allowed_attributes
     [
       :name, :description

24eb9ce6d4a8c14943bd00ac8e67f97fa5a1c193
felipe <fe.valdiviaa@gmail.com>
2015-11-09 22:38:53 -0300

    Fix calendar relationship

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -2,5 +2,5 @@ class Calendar < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
   belongs_to :profile, inverse_of: :calendar
-  belongs_to :booking
+  has_many :bookings
 end

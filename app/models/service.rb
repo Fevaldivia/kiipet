0831abdfd5f3de628859f018dc301d311d8c546f
felipe <fe.valdiviaa@gmail.com>
2015-11-28 16:56:08 -0300

    Add relationship between profile and booking and services and booking

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,6 +3,8 @@ class Service < ActiveRecord::Base
   has_many :profile_services
   has_many :profiles, through: :profile_services
 
+  has_many :bookings
+
   def self.allowed_attributes
     [
       :name, :description

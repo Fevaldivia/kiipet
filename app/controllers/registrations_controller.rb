82eba2ef42906bf4c4f20b93fccb0bf8d7332906
felipe <fe.valdiviaa@gmail.com>
2015-11-28 21:32:56 -0300

    Add link to terms and conditions in register view

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
-
   # The default url to be used after updating a resource. You need to overwrite
   # this method in your own RegistrationsController.
   def after_update_path_for(resource)
     # signed_in_root_path(resource)
-    binding.pry
     edit_user_registration_path
   end
 end

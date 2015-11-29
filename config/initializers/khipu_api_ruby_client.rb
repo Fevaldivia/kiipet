1c287290bd856f59b452dbc34b73afd2c5b487fc
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2015-11-29 19:25:15 -0300

    Setting Up khipu initializer

---
+++ config/initializers/khipu_api_ruby_client.rb
@@ -0,0 +1,6 @@
+Khipu.configure do  |c|
+  c.secret           = ENV['KHIPU_SECRET']
+  c.receiver_id      = ENV['KHIPU_RECEIVER_ID']
+  c.platform         = 'Kiipet'  # (optional) please let us know :)
+  c.platform_version = '0.1'
+end

68d8e42a62a66b4ecc1ad886b7146c92098bd793
felipe <fe.valdiviaa@gmail.com>
2016-01-16 10:14:02 -0300

    Add route for images in paperclip

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,6 @@
-# config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:url] = ':kiipet.s3.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+if Rails.env.production?
+  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+
+  # Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+end

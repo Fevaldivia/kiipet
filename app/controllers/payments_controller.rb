c012393ee1fb0992f4ef9ad3edd3f6750c32294a
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2016-01-30 01:33:09 -0300

    Add changes for csrf token

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,5 @@
 class PaymentsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: [:notify]
   before_action :get_booking, only: [:booking]
   def details
     @payments = Payment.all

2d42ab95969a1dfc234bd82d41e13896578a7551
felipe <fe.valdiviaa@gmail.com>
2016-02-01 01:24:48 -0300

    Fix config rack cors

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module Kiipet
        :global => {'Cache-Control' => 'public, max-age=31536000'},
        :fonts  => {'Access-Control-Allow-Origin' => '*'}
      }
-   end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

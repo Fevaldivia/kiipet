09295cf47bbf565d13f8d3f7b9b6d17ff5c008d8
felipe <fe.valdiviaa@gmail.com>
2016-02-01 02:49:50 -0300

    Add font assets config in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,8 @@ Rails.application.configure do
     end
   }
 
+  config.font_assets.origin = 'http://d26n7qwq5qwjss.cloudfront.net'
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false

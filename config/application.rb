aa6b1c58593cd5d467d05c0aea37d93c9c4609a5
felipe <fe.valdiviaa@gmail.com>
2016-02-01 01:23:21 -0300

    Config my rack cors

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,22 @@ Bundler.require(*Rails.groups)
 
 module Kiipet
   class Application < Rails::Application
-    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: :any
-      end
-    end
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+     allow do
+       origins ''
+       resource '',
+       headers: :any,
+       methods: [:get, :options]
+     end
+   end
+   #…
+     config.assets.paths << Rails.root.join("app", "assets")
+     config.assets.precompile += %w( .svg .eot .woff .ttf )
+     config.assets.header_rules = {
+       :global => {'Cache-Control' => 'public, max-age=31536000'},
+       :fonts  => {'Access-Control-Allow-Origin' => '*'}
+     }
+   end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

b07442527a28d21352fc7c30e0629400bf77fe41
felipe <fe.valdiviaa@gmail.com>
2016-02-02 00:04:48 -0300

    Fix app

--- Gemfile
+++ Gemfile
@@ -16,8 +16,6 @@ group :development, :test do
   gem 'byebug'
   gem 'web-console', '~> 2.0'
   gem 'dotenv-rails'
-  gem 'rspec-rails', '~> 3.0.0'
-  gem 'database_cleaner'
 end
 
 group :assets do
--- spec/models/pet_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe Pet do
-  it "is valid with a name, bio, breed"
-  it "is invalid without a name"
-  it "is invalid without a bio"
-  it "is invalid without a breed"
-end

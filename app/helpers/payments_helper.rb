9626920612990ca3e3e4bcbf2c1e2660b2e498c4
felipe <fe.valdiviaa@gmail.com>
2016-03-04 14:55:25 -0300

    Just fixings errors

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -8,6 +8,15 @@ module PaymentsHelper
     end
   end
 
+  def get_comuna(booking_id)
+    c = Booking.find(booking_id)
+    if c
+      c.calendar.profile
+    else
+      "No encontrado"
+    end
+  end
+
   def get_pagador(profile_id)
     p = Profile.find(profile_id)
 

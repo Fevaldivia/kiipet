c3b55f62bda4789d5ce12a759353c55baf544225
felipe <fe.valdiviaa@gmail.com>
2016-03-05 18:42:49 -0300

    Add some metas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,46 +4,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :prepare_meta_tags, if: "request.get?"
-
-  def prepare_meta_tags(options={})
-    site_name   = "Kiipet"
-    title       = [controller_name, action_name].join(" ")
-    description = "Kiipet es una plataforma que pone en contacto a cuidadores de mascotas con experiencia y propietarios de mascotas.
-                         Nuestra plataforma permite que cada propietario encuentre el cuidador perfecto para su mascota,
-                         haciendo la reserva de forma cómoda y garantizando el pago seguro"
-    image       = options[:image] || "logo-azul.png"
-    current_url = request.url
-
-    # Let's prepare a nice set of defaults
-    defaults = {
-      site:        site_name,
-      title:       title,
-      image:       image,
-      description: description,
-      keywords:    %w[mascotas alojamiento paseos cuidadores veterinarias entrenamiento],
-      twitter: {
-        site_name: site_name,
-        site: '@thecookieshq',
-        card: 'summary',
-        description: description,
-        image: image
-      },
-      og: {
-        url: current_url,
-        site_name: site_name,
-        title: title,
-        image: image,
-        description: description,
-        type: 'website'
-      }
-    }
-
-    options.reverse_merge!(defaults)
-
-    set_meta_tags options
-  end
-
 
 protected
   before_action :configure_permitted_parameters, if: :devise_controller?

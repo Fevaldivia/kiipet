bd50c8be17ff3f059f529f8d3cea749c399a30d9
felipe <fe.valdiviaa@gmail.com>
2016-03-05 21:30:12 -0300

    Add configuration option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   }
 
   config.font_assets.origin = "http://kiipet.com"
+  config.static_cache_control = 'public, max-age=31536000'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

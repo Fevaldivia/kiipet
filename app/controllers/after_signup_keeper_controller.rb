7384271029762339cf8bd75f085e9f44edc41dc6
felipe <fe.valdiviaa@gmail.com>
2016-04-07 09:34:38 -0300

    Add new logic for step by step keeper profile

--- app/controllers/after_signup_keeper_controller.rb
+++ app/controllers/after_signup_keeper_controller.rb
@@ -2,7 +2,7 @@ class AfterSignupKeeperController < ApplicationController
   before_action :authenticate_user!
 
   include Wicked::Wizard
-   before_action :set_profile, only: [:show, :update]
+   before_action :set_profile, only: [:show, :update, :set_step1_defaults, :set_step2_defaults, :set_step3_defaults, :set_step4_defaults]
 
   steps :step1, :step2, :step3, :step4
 
@@ -14,11 +14,11 @@ class AfterSignupKeeperController < ApplicationController
     def create
       @profile = current_user.build_profile profile_params_step1
       if @profile.save
-        redirect_to after_signup_keeper_path(next_step, profile_id: @profile)
+        redirect_to wizard_path(next_step, profile_id: @profile)
       else
         render_wizard
       end
-  end
+    end
 
   def show
     send("set_#{step}_defaults") if steps.include? step
@@ -36,21 +36,26 @@ class AfterSignupKeeperController < ApplicationController
     render_wizard @profile
   end
 
-      private
+    private
       def set_step1_defaults
+        @profile = current_user.build_profile
       end
 
       def set_step2_defaults
       end
 
       def set_step3_defaults
+        @services = Service.all
+        @services.each do |service|
+          @profile.profile_services.build(service: service) unless @profile.services.any?
+         end
       end
 
       def set_step4_defaults
       end
 
       def set_profile
-        @profile = current_user.profile.find(params[:id])
+        @profile = current_user.profile
       end
       # Never trust parameters from the scary internet, only allow the white list through.
       def profile_params_step1
@@ -58,19 +63,19 @@ class AfterSignupKeeperController < ApplicationController
       end
 
       def profile_params_step2
-        params.require(:profile).permit()
+        params.require(:profile).permit(Profile.allowed_attributes)
       end
 
       def profile_params_step3
-        params.require(:profile).permit()
+        params.require(:profile).permit(Profile.allowed_attributes)
       end
 
       def profile_params_step4
-        params.require(:profile).permit()
+        params.require(:profile).permit(Profile.allowed_attributes)
       end
 
       def finish_wizard_path
-        profile_path(current_user)
+        profile_dashboard_path(current_user)
       end
 
 end

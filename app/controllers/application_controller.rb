822d6f2e5a42ecec922d31f5aa1abaef0d9e4b94
felipe <fe.valdiviaa@gmail.com>
2016-04-07 09:35:11 -0300

    Add methods for after signin and after signup routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,17 +8,8 @@ class ApplicationController < ActionController::Base
 protected
   before_action :configure_permitted_parameters, if: :devise_controller?
     def configure_permitted_parameters
-     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:role_id, :terms_of_service, :email, :password, :password_confirmation, :role_ids => [], role_attributes: [:id, :name]) }
+     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:role_id, :terms_of_service, :email, :password, :password_confirmation, :role_ids => [], role_attributes: [:id, :name], :profile_ids => [], profile_attributes: [:id, :name, :last_name]) }
 
       devise_parameter_sanitizer.for(:account_update) << :newsletter
     end
-  def after_sign_in_path_for(user)
-    # return the path based on resource
-    if current_user.role.name == "keeper"
-      after_signup_keeper_path(:step1)
-    else
-      sign_in user
-      step_1_wizards_path
-    end
-  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,25 @@ class RegistrationsController < Devise::RegistrationsController
   protected
   # The default url to be used after updating a resource. You need to overwrite
   # this method in your own RegistrationsController.
+  def after_sign_up_path_for(resource)
+     if current_user.role.name == "keeper"
+        new_after_signup_keeper_path(:step1)
+     else
+        new_after_signup_keeper_path(:step1)
+     end
+  end
+
+
+  def after_sign_in_path_for(user)
+    # return the path based on resource
+    if current_user.role.name == "keeper" && current_user.role.name == "owner"
+      profile_dashboard_path(current_user)
+    else
+      sign_in user
+      step_1_wizards_path
+    end
+  end
+
   def after_update_path_for(resource)
     # signed_in_root_path(resource)
     edit_user_registration_path
